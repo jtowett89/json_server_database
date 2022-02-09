5eb00f01a137b1346154ad58ea1a2c2b6c9a3762
Justice Towett <jtowett89@gmail.com>
2022-02-09 08:36:43 +0300

    enable cors

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
-
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
   next();
 });
-
 server.use(auth);
 server.use(middlewares);
 server.use(router);
