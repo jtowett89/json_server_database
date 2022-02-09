e8459b25e51b375566fa2962f23c0060d87c6ab7
Justice Towett <jtowett89@gmail.com>
2022-02-09 07:25:40 +0300

    add json-server-auth dependency

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults("./node_modules/json-server-auth");
 
 server.use(middlewares);
 server.use(router);
